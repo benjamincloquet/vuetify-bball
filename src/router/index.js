5323e4a15d09a1518401d62fb3938176ae2b30ef
benjamincloquet <benjamin.cloquet@gmail.com>
2022-05-27 13:24:50 +0200

    Configure router

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   },
+  {
+    path: '/play',
+    name: 'game',
+    component: () => import('../views/GameView.vue'),
+  },
 ];
 
 const router = new VueRouter({
