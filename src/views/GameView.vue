54ba1e156f9301639838588a5719257247f3aa7f
benjamincloquet <benjamin.cloquet@gmail.com>
2022-05-27 13:53:23 +0200

    Add vuetify

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        Hi
+        Play!
     </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="home">
+<div class="home">
+    <h1>This is the home page</h1>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HomeView',
 };
